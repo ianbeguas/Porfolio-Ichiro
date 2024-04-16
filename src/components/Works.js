2a4564fe3306a6db5bd25348fc21b77f8416e170
ianbeguas <ianbeguas@gmail.com>
2024-04-16 21:27:44 +0800

    changes

--- src/components/Works.js
+++ src/components/Works.js
@@ -17,7 +17,7 @@ const Works = () => {
                     </div>
                     <div className='grid grid-cols-2 gap-4 sm:grid-cols-3 md:gap-6 xl:gap-8'>
     <a href='https://ianbeguas.github.io/IchiroNissan.github.io/' className='group h-48 overflow-hidden rounded-lg shadow-lg md:h-80 focus:outline-none focus:ring-2 focus:ring-blue-500'>
-        <img src='https://www.nissanusa.com/content/dam/Nissan/us/vehicles/kicks/2024/overview/2024-nissan-kicks-orange.jpg' alt='car pic' className='h-full w-full object-cover object-center transition duration-200 transform group-hover:scale-110 filter blur-md hover:blur-none focus:blur-none'/>
+        <img src='https://www.nissanusa.com/content/dam/Nissan/us/vehicles/kicks/2024/overview/2024-nissan-kicks-orange.jpg' alt='car w' className='h-full w-full object-cover object-center transition duration-200 transform group-hover:scale-110 filter blur-md hover:blur-none focus:blur-none'/>
     </a>
     <a href='https://ianbeguas.github.io/IchiroPortfolio.github.io/' className='group h-48 overflow-hidden rounded-lg shadow-lg md:col-span-2 md:h-80 focus:outline-none focus:ring-2 focus:ring-blue-500'>
         <img src='https://lirp.cdn-website.com/078977fd/dms3rep/multi/opt/2024-trax-masthead-01-2880w.jpg' alt='car pic' className='h-full w-full object-cover object-center transition duration-200 transform group-hover:scale-110 filter blur-md hover:blur-none focus:blur-none'/>
@@ -26,7 +26,7 @@ const Works = () => {
         <img src='https://lirp.cdn-website.com/078977fd/dms3rep/multi/opt/2024-trax-masthead-01-2880w.jpg' alt='car pic' className='h-full w-full object-cover object-center transition duration-200 transform group-hover:scale-110 filter blur-md hover:blur-none focus:blur-none'/>
     </a>
     <a href='https://ianbeguas.github.io/IchiroNissan.github.io/' className='group h-48 overflow-hidden rounded-lg shadow-lg md:h-80 focus:outline-none focus:ring-2 focus:ring-blue-500'>
-        <img src='https://www.nissanusa.com/content/dam/Nissan/us/vehicles/kicks/2024/overview/2024-nissan-kicks-orange.jpg' alt='car pic' className='h-full w-full object-cover object-center transition duration-200 transform group-hover:scale-110 filter blur-md hover:blur-none focus:blur-none'/>
+        <img src='https://www.nissanusa.com/content/dam/Nissan/us/vehicles/kicks/2024/overview/2024-nissan-kicks-orange.jpg' alt='car w' className='h-full w-full object-cover object-center transition duration-200 transform group-hover:scale-110 filter blur-md hover:blur-none focus:blur-none'/>
     </a>
 </div>
 
